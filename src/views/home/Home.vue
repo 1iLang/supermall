e6577f6adc2bdcc123973b7edda0a5ac21964148
lilang <995914589@qq.com>
2021-03-08 16:21:23 +0800

    2021.3.8

---
+++ src/views/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="home">
+    <nav-bar class="nav-set"><div slot="center">购物街</div></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultiData} from 'network/home.js'
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    //请求多个数据
+    getHomeMultiData().then(res => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  } 
+}
+</script>
+
+<style scoped>
+  .nav-set{
+    color: #fff;
+    background: var(--color-tint);
+  }
+</style>

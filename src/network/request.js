4da42837bad349925cac4cbe7391bee3df5b4fd7
lilang <995914589@qq.com>
2022-08-25 14:17:59 +0800

    change

--- src/network/request.js
+++ src/network/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import $ from '@/common/jquery-3.6.0.min.js'
+// import $ from '@/common/jquery-3.6.0.min.js'
 
 //第一种封装
 export function request(config) {
@@ -28,9 +28,10 @@ export function request(config) {
 }
 
 export function requestM(config) {
-  if (process.env.NODE_ENV !== 'production') {
+  // if (process.env.NODE_ENV !== 'production') {
     // 1.创建axios的实例
     const instance = axios.create({
+     // baseURL: process.env.NODE_ENV === 'production'?'https://list.mogujie.com/':'/api',
       baseURL: '/api',
       timeout: 5000
     })
@@ -43,18 +44,18 @@ export function requestM(config) {
     })
     //发送真正的网络请求
     return instance(config)
-  } else {
-    return new Promise((resolve, reject) => {
-      $.ajax({ //生产时暂时使用jquery ajax
-        type: "get",
-        url: "https://list.mogujie.com/" + config.url,
-        data: {
-          sort: config.params.sort,
-          page: config.params.page
-        },
-        dataType: "jsonp",
-        jsonp: "callback"
-      }).done(resolve).fail(reject);
-    })
-  }
+//  } else {
+//     return new Promise((resolve, reject) => {
+//       $.ajax({ //生产时暂时使用jquery ajax,如果配置了nginx可以注释掉else这一段
+//         type: "get",
+//         url: "https://list.mogujie.com/" + config.url,
+//         data: {
+//           sort: config.params.sort,
+//           page: config.params.page
+//         },
+//         dataType: "jsonp",
+//         jsonp: "callback"
+//       }).done(resolve).fail(reject);
+//     })
+//   }
 }
